82dcde4660fb0ec1aa9a4d3aef958be19eaab546
Sidharthsidd <sidh22cs@cmrit.ac.in>
2024-10-01 19:18:00 +0530

    updated

--- src/components/Addtodo.jsx
+++ src/components/Addtodo.jsx
@@ -12,7 +12,7 @@ const addTodoHandler=(e)=>{
 }
     return (
         <form onSubmit ={addTodoHandler} className="space-x-3 mt-12">
-            <div className="bg-yellow-400 "> 
+            <div className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"> 
             <input type="text" className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus :ring-2 focus:ring-indigo-900 text-base outline-none text-gray- bg-green--400" placeeholder="enetra todo ..." value={input} onChange={(e)=>setInput(e.target.value)}/></div> 
             <button type="submit" className="text-white bg-indigo-500 border-0 py-2 px-2 focus:outline-none hover:bbg-indigo-600 rounded text-lg">Add todo</button>
         </form>
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -7,9 +7,9 @@ function Todo(){
     const todos=useSelector(state=>state.todo)  // to access store the we need to use the custom hook useSelector( )
     return (
     <>
-        <div>Todos</div>
+        <div className="text-white">Todos</div>
         {todos.map((todo)=>(
-            <li className=" bg-yellow-600" key={todo.id}>{todo.text}<button onClick={()=>dispatch(removeTodo(todo.id))}>delete</button></li>
+            <li className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded" key={todo.id}>{todo.text}<button onClick={()=>dispatch(removeTodo(todo.id))} className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md">delete</button></li>
         ))}
         </>
     )
